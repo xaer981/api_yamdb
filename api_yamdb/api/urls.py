eba74113031e6e39809e6c6aeb383c852b51aaad
xaer981 <xaer981@yandex.ru>
2023-03-25 19:09:05 +0300

    fixing pytest

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from rest_framework import routers
 
 from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                    ReviewViewSet, TitleViewSet, UserViewSet, get_token,
-                    UserCreateViewSet)
+                    ReviewViewSet, TitleViewSet, UserCreateViewSet,
+                    UserViewSet, get_token)
 
 app_name = 'api'
 
